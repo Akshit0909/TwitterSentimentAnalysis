4b082d5e8e926d72f312f74688ba169d59e2222c
Akshit0909 <akshit.miglani09@gmail.com>
2017-05-18 20:07:01 +0530

    adding

---
+++ README.md
@@ -0,0 +1,6 @@
+# TwitterSentimentAnalysis
+<<<<<<< HEAD
+Real time twitter sentiment analysis shown on a graph chart using NLP + basic java.
+=======
+Sentiment score analysis of tweets. Higher the core, happier the tweet. Major GUI integrations coming soon
+>>>>>>> adding project
---
+++ src/MainActivity.java
@@ -0,0 +1,67 @@
+import org.jfree.chart.ChartFactory;
+import org.jfree.chart.ChartPanel;
+import org.jfree.chart.JFreeChart;
+import org.jfree.data.general.DefaultPieDataset;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+import java.awt.image.BufferedImage;
+import java.util.ArrayList;
+
+
+public class MainActivity extends JFrame{
+
+    static JFrame frame = new JFrame();
+    private JPanel panel;
+    public static void main(String[] args)
+    {
+        String topic="Akshit Miglani";
+        ArrayList<String> tweets = TweetManager.getTweets(topic);
+        NLP.Init();
+        int sentimentCount1 = 0,sentimentCount2=0,sentimentCount3=0;
+        for (String tweet : tweets)
+        {
+            System.out.println(tweet + " : " + NLP.findSentiment(tweet));
+            if(NLP.findSentiment(tweet) == 1)
+                sentimentCount1++;
+            else if(NLP.findSentiment(tweet) == 2 )
+                sentimentCount2++;
+            else if(NLP.findSentiment(tweet) == 3)
+                sentimentCount3++;
+        }
+
+        System.out.println("Tweets with score 1 : " + sentimentCount1);
+        System.out.println("Tweets with score 2 : " + sentimentCount2);
+        System.out.println("Tweets with score 3 : " + sentimentCount3);
+
+        DefaultPieDataset pieDataset = new DefaultPieDataset();
+        pieDataset.setValue("Wretched",sentimentCount1);
+        pieDataset.setValue("Happy",sentimentCount2);
+        pieDataset.setValue("Cloud Nine",sentimentCount3);
+
+        JFreeChart chart = ChartFactory.createPieChart("Twitter Sentiment Analysis- Akshit Miglani",pieDataset);
+
+        BufferedImage image = chart.createBufferedImage(1350,700);
+        JLabel labelChart = new JLabel();
+        labelChart.setIcon(new ImageIcon(image));
+
+        frame.setExtendedState(JFrame.MAXIMIZED_BOTH);
+        frame.setUndecorated(true);
+        frame.setLayout(new GridLayout(3,1));
+        frame.addWindowListener(new WindowAdapter() {
+            @Override
+            public void windowClosing(WindowEvent e) {
+                super.windowClosing(e);
+            }
+        });
+
+        frame.setContentPane(new MainActivity().getContentPane());
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+        frame.add(labelChart);
+        frame.setVisible(true);
+
+    }
+}
